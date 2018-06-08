38b273f8dde8537d75dde4b9c76ec3f9285a6859
liuzhuangsx057 <zhuang.l@winbaoxian.com>
2018-06-08 15:35:05 +0800

    modified ui

--- voice-calculator/embededTableViewController.swift
+++ voice-calculator/embededTableViewController.swift
@@ -12,15 +12,19 @@ class embededTableViewController: UITableViewController {
     @IBOutlet weak var lightModeLabel: UILabel!
     @IBOutlet weak var versionNumberLabel: UILabel!
     @IBOutlet weak var lightModeCell: UIView!
+    @IBOutlet weak var rateUsCell: UIView!
     @IBOutlet weak var versionNumberCell: UIView!
     @IBOutlet weak var feedbackCell: UIView!
     @IBOutlet weak var shareCell: UIView!
     @IBOutlet weak var lightModeSwitch: UISwitch!
+    @IBOutlet weak var rateUsButton: UIButton!
+    
     @IBAction func lightModeSwitched(_ sender: UISwitch) {
         if  SettingsService.sharedService.lightModeStatus {
             SettingsService.sharedService.lightModeStatus = false
             SettingsService.sharedService.backgroundColor = UIColor.black
             self.view.backgroundColor = SettingsService.sharedService.backgroundColor
+            rateUsCell.backgroundColor = SettingsService.sharedService.backgroundColor
             lightModeCell.backgroundColor = SettingsService.sharedService.backgroundColor
             versionNumberCell.backgroundColor = SettingsService.sharedService.backgroundColor
             feedbackCell.backgroundColor = SettingsService.sharedService.backgroundColor
@@ -28,10 +32,12 @@ class embededTableViewController: UITableViewController {
             SettingsService.sharedService.textColor = UIColor.white
             lightModeLabel.textColor = SettingsService.sharedService.textColor
             versionNumberLabel.textColor = SettingsService.sharedService.textColor
+            rateUsButton.titleLabel?.textColor = SettingsService.sharedService.textColor
         } else {
             SettingsService.sharedService.lightModeStatus = true
             SettingsService.sharedService.backgroundColor = UIColor.white
             self.view.backgroundColor = SettingsService.sharedService.backgroundColor
+            rateUsCell.backgroundColor = SettingsService.sharedService.backgroundColor
             lightModeCell.backgroundColor = SettingsService.sharedService.backgroundColor
             versionNumberCell.backgroundColor = SettingsService.sharedService.backgroundColor
             feedbackCell.backgroundColor = SettingsService.sharedService.backgroundColor
@@ -39,8 +45,13 @@ class embededTableViewController: UITableViewController {
             SettingsService.sharedService.textColor = UIColor.black
             lightModeLabel.textColor = SettingsService.sharedService.textColor
             versionNumberLabel.textColor = SettingsService.sharedService.textColor
-            
+            rateUsButton.titleLabel?.textColor = SettingsService.sharedService.textColor
         }
+    }
+    
+    
+    @IBAction func rateUsPressed(_ sender: UIButton) {
+        gotoAppStore()
     }
     
     override func viewDidLoad() {
@@ -53,6 +64,8 @@ class embededTableViewController: UITableViewController {
         feedbackCell.backgroundColor = SettingsService.sharedService.backgroundColor
         shareCell.backgroundColor = SettingsService.sharedService.backgroundColor
         lightModeSwitch.isOn = SettingsService.sharedService.lightModeStatus
+        rateUsButton.titleLabel?.textColor = SettingsService.sharedService.textColor
+        rateUsCell.backgroundColor = SettingsService.sharedService.backgroundColor
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -69,7 +82,21 @@ class embededTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func gotoAppStore() {
+        let urlString = "itms-apps://itunes.apple.com/cn/app/%E4%BF%9D%E9%99%A9%E5%B8%88-%E8%AE%A1%E5%88%92%E4%B9%A6-%E5%B1%95%E4%B8%9A%E7%AD%BE%E5%8D%95%E5%B9%B3%E5%8F%B0/id981360164?l=en&mt=8"
+        if let url = URL(string: urlString) {
+            
+            if #available(iOS 10, *) {
+                UIApplication.shared.open(url, options: [:],
+                                          completionHandler: {
+                                            (success) in
+                })
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
+    }
     // MARK: - Table view data source
 
 //    override func numberOfSections(in tableView: UITableView) -> Int {
